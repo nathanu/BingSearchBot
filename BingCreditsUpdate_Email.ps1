189e66a235c6ad34a8f444eecef19cdd7787f99d
nathanu <nathanurbanowski@gmail.com>
2015-03-20 09:37:53 -0400

    Add first two scripts
    These send updated point totals via email and Pushover.

---
+++ BingCreditsUpdate_Email.ps1
@@ -0,0 +1,10 @@
+﻿$From = "nathanurbanowski@gmail.com"
+$To = "nathanurbanowski+bing@gmail.com"
+$Subject = "Bing Status"
+$Body = [IO.File]::ReadAllText("C:\Program Files (x86)\BingSearchBot\BingCredits.txt")
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = "587"
+$Password = get-content 'C:\Program Files (x86)\BingSearchBot\cred.txt' | convertto-securestring
+$Credential = New-Object System.Management.Automation.PSCredential("nathanurbanowski@gmail.com",$Password)
+
+Send-MailMessage -From $From -to $To -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential $Credential
---
+++ BingCreditsUpdate_Pushover.ps1
@@ -0,0 +1,7 @@
+﻿$uri = "https://api.pushover.net/1/messages.json"
+$parameters = @{
+  token = "aKwdCGbw8zSCCuSKqPkxJee11PS9SX"
+  user = "uLiMkDVhfFq7hkNk69dbxYsUFgcLmm"
+  message = [IO.File]::ReadAllText("C:\Program Files (x86)\BingSearchBot\BingCredits.txt")
+}
+$parameters | Invoke-RestMethod -Uri $uri -Method Post
